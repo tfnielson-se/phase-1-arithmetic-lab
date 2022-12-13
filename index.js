0d6ae42e93808e940593bcaf3b423e0f15e8afef
Thomas <tfnielson.se@gmail.com>
2022-12-13 09:58:13 -0700

    commit tree a6d36ee484e00c7a96c4fffee06e00385bfdece0

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
+const num1 = 31;
+const num2 = 2;
+const num3 = 6;
+const num4 = 2;
+const array1 = [1, 2, 3, 10, 20];
 
-// Write your code here
+function multiply () {
+    let multi = (num1 * num2);
+    return multi;
+};
+
+multiply;
+
+
+function random () {
+    let randInt = Math.ceil(Math.random() * 10) + 1;
+    return randInt
+};
+
+function mod () {
+    let remain = (num3 - num4)
+    return remain
+};
+
+
+function max () {
+    maxNum = Math.max(array1)
+    return maxNum
+};
